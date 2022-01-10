9b20bcc29bb7643916f167bc953fb9ef9ff665c0
Erick Guzman <guzmi.dev@gmail.com>
2022-01-09 17:47:09 -0700

    Paquete excepciones creada

---
+++ src/mx/com/gm/peliculas/excepeciones/EscriturasDatosEx.java
@@ -0,0 +1,9 @@
+package mx.com.gm.peliculas.excepeciones;
+
+public class EscriturasDatosEx extends AccesoDatosEx{
+    
+    public EscriturasDatosEx(String mensaje) {
+        super(mensaje);
+    }
+    
+}
---
+++ src/mx/com/gm/peliculas/excepeciones/LecturaDatosEx.java
@@ -0,0 +1,9 @@
+package mx.com.gm.peliculas.excepeciones;
+
+public class LecturaDatosEx extends AccesoDatosEx{
+    
+    public LecturaDatosEx(String mensaje) {
+        super(mensaje);
+    }
+    
+}
